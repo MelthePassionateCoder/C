8086b4f810f10cd8ca52866d95d16da6d4f6a49f
Mel-liza31 <limbagomelliza31@gmail.com>
2024-08-24 10:56:53 +0800

    exercise problem #2 where u will output a string

---
+++ problem2.c
@@ -0,0 +1,12 @@
+/* Write a C program that declares a variable of type 'char[]'
+    to store your name and then outputs a greeting that includes your name.
+    Use the printf function
+*/
+
+#include <stdio.h>
+
+int main() {
+    char name[] = "Mel Liza";
+
+    printf("Hello, %s!", name );
+}
