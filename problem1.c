4e9956bd6f2d6faaa8e1ab46fa8662cf50463080
Mel-liza31 <limbagomelliza31@gmail.com>
2024-08-24 10:26:56 +0800

    first commit

---
+++ problem1.c
@@ -0,0 +1,12 @@
+// Create a simple program that prints "Hello, C!" to the console
+/* Instructions:
+    Include the necessary header file
+    Use the printf function to output the message
+    Add a comment explaining what the program does
+*/
+
+#include <stdio.h>
+
+int main() {
+printf("Hello, C!");
+}
