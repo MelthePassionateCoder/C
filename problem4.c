3b9041b5fd3c39c9e99becd9ab37b7c82892e5ed
Mel-liza31 <limbagomelliza31@gmail.com>
2024-08-25 06:09:06 +0800

    a program that takes two integers from user input and perform arithmetic operations on that numbers

---
+++ problem4.c
@@ -0,0 +1,25 @@
+/* Write a C program that takes two integers as input from the user. 
+    Perform addition, subtraction, multiplication, division and modulus operations on
+    two numbers.
+    Print the results of each operation.
+*/
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    float quotient;
+
+    printf("Enter a first number: \n");
+    scanf("%d", &num1);
+    printf("Enter a second number: \n");
+    scanf("%d", &num2);
+
+    printf("Sum: %d\n", num1+num2);
+    printf("Difference: %d\n", num1-num2);
+    printf("Product: %d\n", num1*num2);
+    quotient = (float) num1 / num2;
+    printf("Quotient: %.2f\n", quotient);
+    printf("Modulo: %d", num1%num2);
+    return 0;
+
+}
